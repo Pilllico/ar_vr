0a8eadf6565aede6562ccac15726031a07c46100
Pilllico <douglas1995@hotmail.fr>
2020-10-22 15:46:26 +0200

    Tried a fix

---
+++ VR_VA_TP4_CARDBOARD/Assets/MyScripts/PlayerBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviourScript : MonoBehaviour
+{
+    public static bool walking = true;
+    private Vector3 spawnPoint;
+    private Camera mainCamera;
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        spawnPoint = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (walking)
+        {
+            transform.position = transform.position + mainCamera.transform.forward * speed * Time.deltaTime;
+        }
+        if (transform.position.y < -10f)
+        {
+            transform.position = spawnPoint;
+        }
+    }
+}
---
+++ VR_VA_TP4_VUFORIA/Assets/MyScripts/animObj.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animObj : MonoBehaviour
+{
+    private Animation anim;
+    public string anim_walk;
+    public string anim_idle;
+    public string anim_name;
+    public int size;
+    private int s = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void animate_start()
+    {
+        anim.Play(anim_walk);
+    }
+
+    public void animate_stop()
+    {
+        anim.Play(anim_idle);
+    }
+
+    public void switch_anim()
+    {
+        s = (s + 1) % (size + 1); 
+        if (s == 0) s++;
+        string a = anim_name;
+        if (s < 10) a += "0";
+        a += s.ToString();
+        anim.Play(a);
+    }
+}
